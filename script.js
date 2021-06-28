88a6d86eeec81d25d824e4134214890cae07a617
mathigatti <mathigatti@gmail.com>
2021-06-28 00:08:00 -0300

    new table and form

--- script.js
+++ script.js
@@ -1,132 +1,81 @@
-const categoryStartNum = 3; // let the program know where the categoy begins on the spreadsheet column. Default value is 3.
-const sheetName = "Sheet1"; // this has to match your google doc sheet name
-const punctuation = ","; // this changes the punctuation between the title and the description. In most cases you'd want to use "," or "-" or ":"
 
-// tableTop.js script
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1QQKTVEIHFY1OTlYWXgCE2Nw1JrcnKOLARkGtGAPoF5o/edit?usp=sharing';
+var sheet_data;
+
 function init() {
-  Tabletop.init({
-    key: publicSpreadsheetUrl,
-    callback: showInfo,
-    simpleSheet: true
-  });
+  Tabletop.init( { key: publicSpreadsheetUrl,
+                   callback: loadSheet,
+                   simpleSheet: true } )
 }
 
-function showInfo(data, tabletop) {
-  const checked = "x";
-  const columnArray = tabletop.sheets()[sheetName].columnNames;
-    const columnName = [columnArray.length];
+function loadSheet(data, tabletop) {
+  var data_processed = data.map( x => [x["Título"], x["Link"], x["Descripción"], x["Provincia"], x["Categorías"], x["Localidad"]] )
 
-    console.log(columnArray);
+  var table = $('#example').DataTable({
+      "bPaginate": false,
+      "bLengthChange": false,
+      "bFilter": true,
+      "bInfo": false,
+      "bAutoWidth": false,
 
-  for (let j = 0; j < columnArray.length; j++) {
-    columnName[j] = columnArray[j];
-  }
+      "data" : data_processed,
+      "column": [
+      { "data" : "Titulo" },
+      { "data" : "Link" },
+      { "data" : "Descripción" },
+      { "data" : "Provincia" },
+      { "data" : "Categorías" },      
+      { "data" : "Localidad" },      
+      ],
+      "columnDefs": [
+          {
+              "targets": [3],
+              "visible": false
+          },
+          {
+              "targets": [4],
+              "visible": false
+          },
+          {
+              "targets": [5],
+              "visible": false
+          },
+      ],
+   });
 
-  // create sorting buttons
-  for (let j = categoryStartNum; j < columnArray.length; j++) {
-    addButton(columnName[j]);
-  }
 
-  for (let i = categoryStartNum; i < columnArray.length; i++) {
-    for (let j = 0; j < data.length; j++) {
-      if (data[j][columnName[i]] == checked) {
-        addElement(
-          columnName[i],
-          data[j][columnName[0]],
-          data[j][columnName[1]],
-          data[j][columnName[2]]
-        );
+  var indices = [3,4]
+  $("#search b").each( function ( i ) {
+      i = indices[i];
+
+      if ($(this).text() !== '') {
+          var isStatusColumn = (($(this).text() == 'Status') ? true : false);
+          var select = $('<select><option value="">'+$(this).text()+'</option></select>')
+              .appendTo( $(this).empty() )
+              .on( 'change', function () {
+                  var val = $(this).val();
+                  
+                  table.column( i )
+                      //.search( val ? '^'+$(this).val()+'$' : val, true, false ) // Soporta regex esto ;)
+                      .search( val ? $(this).val() : val, true, false )
+                      .draw();
+              } );
+          
+          var values = [];
+
+          table.column( i ).data().unique().sort().each( function ( d, j ) {  
+              d.split(", ").map( d => values.push(d))
+          });
+
+          values = [...new Set(values)];
+
+          values.map( function ( d, j ) {
+              select.append( '<option value="'+d+'">'+d+'</option>' );
+          } );
+
       }
-    }
-  }
-  
+  } );
+
 }
 
-function addButton(columnName) {
-  const newButton = document.createElement("a");
-  const newButtonContent = document.createTextNode(columnName);
-
-  newButton.appendChild(newButtonContent);
-  newButton.className = "btn";
-  newButton.addEventListener("click", function() {
-    filterSelection(columnName);
-    btnOff(); // turns off all active buttons
-    newButton.classList.add("active"); // turn this button on
-  });
-  document.getElementById("myBtnContainer").appendChild(newButton);
-}
-
-function btnOff() {
-  let btnClassArray = document.getElementsByClassName("btn");
-  for (let i = 0; i < btnClassArray.length; i++) {
-    if (btnClassArray[i].classList.contains("active")) {
-      btnClassArray[i].classList.remove("active");
-    }
-  }
-}
-
-function addElement(columnName, person, url, description) {
-  const hashtag1 = ["filterDiv"];
-  const hashtag2 = [columnName];
-  const hashtagArray = hashtag1.concat(hashtag2);
-  const hashtagString = hashtagArray.join(" ");
-  const newDiv = document.createElement("div");
-  newDiv.className = hashtagString;
-
-  // place individual link inside individual div
-  for (let i = 0; i < 1; i++) {
-    let link = document.createElement("a");
-    let linkContent = document.createTextNode(person);
-    link.appendChild(linkContent);
-    link.title = person;
-    link.href = url;
-    link.target="_blank";
-    link.className = "stretched-link";
-
-    let para = document.createElement("p");
-    let paraContent = document.createTextNode(`${punctuation} ${description}`);
-    para.appendChild(paraContent);
-    para.className = "card-text";
-
-    para.appendChild(link); // put <a> into <p>
-    link.after(paraContent); // put <p> description after <a>
-    newDiv.appendChild(para); // put <p> into newDiv
-  }
-  document.getElementsByClassName("container")[0].appendChild(newDiv);
-}
-
-window.addEventListener("DOMContentLoaded", init);
-
-// filter script
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
-
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
-
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
+init();
